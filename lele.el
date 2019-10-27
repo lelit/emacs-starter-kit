92736dc345872d3aad5c1bfae6c4403c46c55359
Lele Gaifax <lele@metapensiero.it>
2019-10-27 10:38:35 +0100

    Enable upcase-region

--- lele.el
+++ lele.el
@@ -111,6 +111,7 @@ start everything unconditionally."
 
 (put 'narrow-to-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
 
 
 ;; Customize will write the settings here
